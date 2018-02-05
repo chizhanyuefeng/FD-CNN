242c65d3cda0b9282518e3690a27acfe69ffb841
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-02-05 16:31:49 +0800

    完善代码工作

--- utils/data_graph.py
+++ utils/data_graph.py
@@ -1,23 +1,32 @@
 # -*- coding:UTF-8 -*-
 
-
+import os
 import matplotlib.pyplot as plt
 import pandas as pd
 import file_io as fio
 import numpy as np
 
 def line_chart(csv_file):
-    data = pd.read_csv(csv_file)
+    if os.path.exists(csv_file) == False:
+        print("文件不存在！")
+        return csv_file
 
+    data = pd.read_csv(csv_file)
     num = data.timestamp.size
+
     x = np.arange(num)
-    plt.plot(x, data.ax, label="x")
-    plt.plot(x, data.ay, label="y")
-    plt.plot(x, data.az, label="z")
+    if 'acc' in csv_file:
+        plt.plot(x, data.ax, label="x")
+        plt.plot(x, data.ay, label="y")
+        plt.plot(x, data.az, label="z")
+    elif 'gyro' in csv_file:
+        plt.plot(x, data.gx, label="x")
+        plt.plot(x, data.gy, label="y")
+        plt.plot(x, data.gz, label="z")
+    # 添加解释图标
     plt.legend()
     plt.xticks([0,100,200,300,400,500,600,700,800])
     plt.show()
-    #data.plot()
 
 def main():
     """
