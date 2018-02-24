626515056295e26292c34a93d3f29b7a87e094b8
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-02-24 12:38:06 +0800

    截取完sub2/BSC

--- utils/file_io.py
+++ utils/file_io.py
@@ -130,7 +130,7 @@ def find_txt_data_file(path):
 
 
 def main():
-    path = '/home/tony/fall_data/MobiFall_Dataset_v2.0/sub2/FALLS/SDL/'
+    path = '/home/tony/fall_data/MobiFall_Dataset_v2.0/sub2/FALLS/BSC/'
 
     for i in os.listdir(path):
         file = path + '/' + i
