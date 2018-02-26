61613580e5063eb5d03df2b0d2d632094d46264c
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-02-26 10:54:39 +0800

    截取完fall down sub17 数据

--- utils/file_io.py
+++ utils/file_io.py
@@ -130,7 +130,7 @@ def find_txt_data_file(path):
 
 
 def main():
-    path = '/home/tony/fall_data/MobiFall_Dataset_v2.0/sub16/FALLS/SDL/'
+    path = '/home/tony/fall_data/MobiFall_Dataset_v2.0/sub17/FALLS/SDL/'
 
     for i in os.listdir(path):
         file = path + '/' + i
