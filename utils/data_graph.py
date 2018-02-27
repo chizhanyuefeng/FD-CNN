8ec598aaa96bdafe4e15530b4232d8a5f713f87a
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-02-27 17:12:49 +0800

    给图表添加label标签，更好的显示图表信息

--- utils/data_graph.py
+++ utils/data_graph.py
@@ -3,7 +3,6 @@
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
-#import file_io as fio
 import numpy as np
 
 def fall_line_chart(csv_file):
@@ -45,8 +44,15 @@ def adl_line_chart(csv_file):
     data = pd.read_csv(csv_file)
     num = data.timestamp.size
 
-    x = np.arange(num)
+    # 获取标签和其最终的位置
+    label_dict = {}
+    for i in range(num):
+        label_dict[data.label[i]] = i
+    print(len(label_dict))
+    label_dict_keys = label_dict.keys()
+    print(label_dict.keys())
 
+    x = np.arange(num)
     plt.figure(1)
     # 子表1绘制加速度传感器数据
     plt.subplot(211)
@@ -54,16 +60,33 @@ def adl_line_chart(csv_file):
     plt.plot(x, data.acc_x, label='x')
     plt.plot(x, data.acc_y, label='y')
     plt.plot(x, data.acc_z, label='z')
+
+    acc_max = max(data.acc_x.max(),data.acc_y.max(),data.acc_z.max())
+    acc_min = min(data.acc_x.min(),data.acc_y.min(),data.acc_z.min())
+
+    acc_y = np.arange(acc_min,acc_max,0.1)
+    for key in label_dict:
+        value = np.zeros((acc_y.size)) +label_dict[key]
+        plt.plot(value, acc_y, label=key)
+
     # 添加解释图标
     plt.legend()
     plt.xticks([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200])
 
     # 子表2绘制陀螺仪传感器数据
     plt.subplot(212)
-    plt.title('groy')
+    plt.title('gyro')
     plt.plot(x, data.gyro_x, label='x')
     plt.plot(x, data.gyro_y, label='y')
     plt.plot(x, data.gyro_z, label='z')
+
+    gyro_max = max(data.gyro_x.max(),data.gyro_y.max(),data.gyro_z.max())
+    gyro_min = min(data.gyro_x.min(),data.gyro_y.min(),data.gyro_z.min())
+
+    gyro_y = np.arange(gyro_min,gyro_max,0.1)
+    for key in label_dict:
+        value = np.zeros((gyro_y.size)) +label_dict[key]
+        plt.plot(value, gyro_y, label=key)
     # 添加解释图标
     plt.legend()
     plt.xticks([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200])
