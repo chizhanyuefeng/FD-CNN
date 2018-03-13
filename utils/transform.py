f38b0fa0ac24a744219280a21babf5cfe9bf0a84
chizhanyuefeng <chizhanyuefeng@gmail.com>
2018-03-13 11:59:06 +0800

    测试数据转化图片算法

--- utils/transform.py
+++ utils/transform.py
@@ -7,35 +7,41 @@ import pandas as pd
 
 def transform_sensor_data(sensor_data):
     '''
-    将跌倒数据转化成图像数据
+    将跌倒数据规范化
     :param sensor_data: 长度为1200的数组
     :return:
     '''
     # 初始大小为3*400的矩阵。3为RGB通道
     transform_data = np.zeros(shape=[3,400],dtype=np.int)
-    # re = np.zeros(shape=[3,400],dtype=np.float)
+    re = np.zeros(shape=[3,400],dtype=np.float)
 
     for i in range(400):
         # 传感器数据大小敢为-20~20，需要将其值拓展为0~255的数值
         transform_data[0][i] = (sensor_data[3*i] + 20) * 5 # R通道，传感器x值
         transform_data[1][i] = (sensor_data[3*i+ 1] + 20) * 5 # G通道，传感器y值
         transform_data[2][i] = (sensor_data[3*i+ 2] + 20) * 5 # B通道，传感器z值
-        #
-        # re[0][i] = (sensor_data[3 * i] )  # R通道，传感器x值
-        # re[1][i] = (sensor_data[3 * i + 1])   # G通道，传感器y值
-        # re[2][i] = (sensor_data[3 * i + 2])   # B通道，传感器z值
-    #
-    # x = np.arange(400)
-    # plt.plot(x, transform_data[0], label='x')
-    # plt.plot(x, transform_data[1], label='y')
-    # plt.plot(x, transform_data[2], label='z')
-    # plt.show()
-    #
-    # plt.plot(x, re[0], label='x')
-    # plt.plot(x, re[1], label='y')
-    # plt.plot(x, re[2], label='z')
-    #
-    # plt.show()
+
+        re[0][i] = (sensor_data[3 * i] )  # R通道，传感器x值
+        re[1][i] = (sensor_data[3 * i + 1])   # G通道，传感器y值
+        re[2][i] = (sensor_data[3 * i + 2])   # B通道，传感器z值
+
+    x = np.arange(400)
+
+    fig = plt.figure(1)
+
+    plt.subplot(211)
+    plt.title('tranform')
+    plt.plot(x, transform_data[0], label='x')
+    plt.plot(x, transform_data[1], label='y')
+    plt.plot(x, transform_data[2], label='z')
+
+    plt.subplot(212)
+    plt.title('origin')
+    plt.plot(x, re[0], label='x')
+    plt.plot(x, re[1], label='y')
+    plt.plot(x, re[2], label='z')
+
+    #plt.show()
 
     transform_data = transform_data.reshape([3,20,20])
     return transform_data
@@ -56,7 +62,7 @@ def data2image(transform_data,num):
     b = Image.fromarray(transform_data[2],'L')#.convert('L')
 
     image = Image.merge('RGB',(r,g,b))
-    image.save('/home/tony/git_project/fall_down_detection/data/'+str(num) + '.png','png')
+    image.save('/home/tony/fall_research/fall_down_detection/data/ADL/CHU/'+str(num) + '.png','png')
 
     return image
 
@@ -64,7 +70,7 @@ def data2image(transform_data,num):
 
 if __name__=='__main__':
 
-    fall_data = pd.read_csv('../data/fall_data.csv')
+    fall_data = pd.read_csv('../data/ADL/CHU/extract_CHU_data.csv')
 
     num = fall_data.label.size
 
